f6d07797bad1efa930efb907027e659e022cea21
jcmilena <juancarlos.milena@iesjoandaustria.org>
2019-09-20 17:07:56 +0200

    Segundo commit falla todo.

--- app/src/main/java/com/example/jcmilena/introdual/MainActivity.java
+++ app/src/main/java/com/example/jcmilena/introdual/MainActivity.java
@@ -12,5 +12,6 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         Log.i("DUAL","Primer log de mi App");
+        Log.i("DUAL","Segundo log de mi App");
     }
 }
